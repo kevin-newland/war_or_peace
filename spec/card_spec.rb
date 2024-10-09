126d014537f6af59220cb5111fe9906ff39703d5
Kevin Newland <kevin.newland@yahoo.com>
2024-10-09 14:06:47 -0500

    adding pry

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,20 +1,20 @@
 require 'rspec'
 require './lib/card'
+require 'pry'
 
 RSpec.describe Card do
-  describe '#initialize' do
-    it "exists" do
-      card = Card.new(:diamond, 'Queen', 12)
+  it "exists" do
+    card = Card.new(:diamond, 'Queen', 12)
+     
+    expect(card).to be_an_instance_of(Card)
+  end
+      
+  it "has readable attributes" do
+    card = Card.new(:diamond, 'Queen', 12)
 
-      expect(card).to be_an_instance_of(Card)
-    end
-
-    it "has readable attributes" do
-      card = Card.new(:diamond, 'Queen', 12)
-
-      expect(card.suit).to eq(:diamond)
-      expect(card.value).to eq('Queen')
-      expect(card.rank).to eq(12)
-    end
+    expect(card.suit).to eq(:diamond)
+    expect(card.value).to eq('Queen')
+    expect(card.rank).to eq(12)
+    binding.pry 
   end
 end
