e541a2e19f2e1e7ab381f6cd28b7bafabe319eab
Kevin Newland <kevin.newland@yahoo.com>
2024-10-10 13:01:44 -0500

    created structure to spec file

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe Deck do
       expect(deck.cards).to eq([card1, card2, card3]) 
     end
 
-    it "can find rank" do
+    it "can find rank at a index" do
       card1 = Card.new(:diamond, 'Queen', 12)
       card2 = Card.new(:spade, '3', 3)
       card3 = Card.new(:heart, 'Ace', 14)
@@ -27,5 +27,44 @@ RSpec.describe Deck do
       expect(deck.rank_of_card_at(0)).to eq(12)
       expect(deck.rank_of_card_at(2)).to eq(14)
     end
+
+    it "high ranking cards" do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3] 
+
+      deck = Deck.new(cards)
+
+      
+    end 
+
+    it "percentage high ranking" do 
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3] 
+
+      deck = Deck.new(cards)
+    end
+
+    it "removes card"  do 
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3] 
+
+      deck = Deck.new(cards)
+    end 
+
+    it "adds card" do
+      card1 = Card.new(:diamond, 'Queen', 12)
+      card2 = Card.new(:spade, '3', 3)
+      card3 = Card.new(:heart, 'Ace', 14)
+      cards = [card1, card2, card3] 
+
+      deck = Deck.new(cards)
+    end 
+
   #end
 end
