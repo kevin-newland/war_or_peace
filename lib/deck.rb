ec17b1dd8e54f96b155c0dd9343a9a016d324bbb
Kevin Newland <kevin.newland@yahoo.com>
2024-10-10 13:01:28 -0500

    created structure to file

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,13 +1,29 @@
 class Deck 
   attr_accessor :cards
-                :index
+                :rank
 
   def initialize(cards)
     @cards = cards
 
   end
 
-  def rank_of_card_at(cards)
-    @cards = index.rank
+  def rank_of_card_at(index)
+    @cards[index].rank
   end
+
+  def high_ranking_cards
+    @cards >= 11
+  end
+
+  def percent_high_ranking
+
+  end 
+
+  def remove_card
+
+  end
+
+  def add_card
+
+  end 
 end
