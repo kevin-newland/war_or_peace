55166668d83cad1ef3c26f328b28b128b541e0d4
Kevin Newland <kevin.newland@yahoo.com>
2024-10-11 08:39:00 -0500

    finished iteration 1

--- lib/deck.rb
+++ lib/deck.rb
@@ -1,10 +1,10 @@
 class Deck 
   attr_accessor :cards
                 :rank
+                :value
 
   def initialize(cards)
     @cards = cards
-
   end
 
   def rank_of_card_at(index)
@@ -12,18 +12,20 @@ class Deck
   end
 
   def high_ranking_cards
-    @cards >= 11
+    @cards.select { |card| card.rank >= 11} #change? check docs
   end
 
   def percent_high_ranking
-
+   high_ranking_cards = @cards.select { |card| card.rank >= 11}
+   expected_percentage = (high_ranking_cards.count.to_f/ @cards.count.to_f) * 100 
+   expected_percentage.round(2)
   end 
-
+#equation?
   def remove_card
-
+    @cards.shift
   end
 
-  def add_card
-
+  def add_card(card)
+    @cards << card
   end 
 end
