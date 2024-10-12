2a03086e751824aa5ff9e84ce91d1fd665c3386e
Kevin Newland <kevin.newland@yahoo.com>
2024-10-12 13:32:07 -0500

    comments added

--- lib/player.rb
+++ lib/player.rb
@@ -1,14 +1,14 @@
 class Player 
-  attr_accessor :name, 
+  attr_accessor :name, #allows reading and modifying to be available to the attributes listed 
                 :deck
                 
               
-  def initialize(name, deck)
+  def initialize(name, deck)# takes the name of the player and the players deck in form of arguments
     @name = name
     @deck = deck
   end
 
-  def has_lost?# ? means boolean
-    @deck.cards.empty? 
+  def has_lost? # ? means boolean
+    @deck.cards.empty? # checks to see if the cards in the deck are empty(returns true if empty, false if not)
   end
 end 
